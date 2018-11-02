609df3b41c1d11ab3d2fb95d38637d9e46e5ac1d
Francesco Soncina <phra@users.noreply.github.com>
2018-11-02 11:52:38 +0100

    chore: add --depth=1 when cloning wordlists repos

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mkdir Repositories/
 cd Repositories
-git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
-git clone https://github.com/xmendez/wfuzz.git
-git clone https://github.com/fuzzdb-project/fuzzdb.git
-git clone https://github.com/minimaxir/big-list-of-naughty-strings.git
-git clone https://github.com/foospidy/payloads.git
-git clone https://github.com/danielmiessler/RobotsDisallowed.git
-git clone https://github.com/danielmiessler/SecLists.git
-
+git clone --depth=1 https://github.com/swisskyrepo/PayloadsAllTheThings.git
+git clone --depth=1 https://github.com/xmendez/wfuzz.git
+git clone --depth=1 https://github.com/fuzzdb-project/fuzzdb.git
+git clone --depth=1 https://github.com/minimaxir/big-list-of-naughty-strings.git
+git clone --depth=1 https://github.com/foospidy/payloads.git
+git clone --depth=1 https://github.com/danielmiessler/RobotsDisallowed.git
+git clone --depth=1 https://github.com/danielmiessler/SecLists.git
