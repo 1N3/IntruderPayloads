478d96477607d328312b2ddbd9689ad1b9338f96
xer0dayz <1N3@hushmail.com>
2020-06-10 08:18:53 -0700

    Updated repo update.sh script

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Shoutz to @vay3t
+cd Repositories
+cd PayloadsAllTheThings/ && git pull && cd ..
+cd wfuzz/ && git pull && cd ..
+cd fuzzdb/ && git pull && cd ..
+cd big-list-of-naughty-strings/ && git pull && cd ..
+cd payloads/ && git pull && cd ..
+cd RobotsDisallowed/ && git pull && cd ..
+cd SecLists/ && git pull && cd ..
